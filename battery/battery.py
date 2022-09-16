2e08cf055f2eb4623ded1326f06397d14f21404e
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-09-16 12:29:57 -0400

    Revert "Create battery.py"
    This reverts commit 4ae0b239c08997640793bfb03c27d150b3931d1f.

--- battery/battery.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Battery(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
-
-    @abstractmethod
-    def battery_needs_service(self):
-        pass
