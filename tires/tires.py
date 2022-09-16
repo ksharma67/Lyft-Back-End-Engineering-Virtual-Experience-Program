84bce81a76e83ac457598b075528fb55a65b424b
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-09-16 12:30:07 -0400

    Revert "Update tires.py"
    This reverts commit 2c64acdd432c8c6d5cb07eff9b4afd494d7cf0fb.

--- tires/tires.py
+++ tires/tires.py
@@ -1,9 +1,6 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
-class Tire(ABC):
-    def __init__(self, tire_sensor_data):
-        self.tire_sensor_data = tire_sensor_data
 
-    @abstractmethod
-    def tire_needs_service(self):
+class Tires(ABC):
+    def needs_service(self):
         pass
