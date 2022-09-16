52fd3fa596aeab807c8636edb3f379c1935faab5
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-09-16 00:01:25 -0400

    Delete tire.py

--- tire.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Tire(ABC):
-    def __init__(self, tire_sensor_data):
-        self.tire_sensor_data = tire_sensor_data
-
-    @abstractmethod
-    def tire_needs_service(self):
-        pass
