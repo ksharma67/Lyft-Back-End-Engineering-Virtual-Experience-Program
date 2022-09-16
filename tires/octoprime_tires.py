f49b8947f8d3162db5ba955a66d9945077948e07
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2022-09-16 12:30:11 -0400

    Revert "Update octoprime_tires.py"
    This reverts commit 8039499e963b71aebb01143d7abeadab48782318.

--- tires/octoprime_tires.py
+++ tires/octoprime_tires.py
@@ -1,11 +1,9 @@
-from tire import Tire
+from tires.tires import Tires
 
-class OctoprimeTire(Tire):
-    def __init__(self, tire_sensor_data):
-        super().__init__(tire_sensor_data)
-        self.tire_sensor_data = tire_sensor_data
 
-    def tire_needs_service(self):
-        # lambda function to find sum of all values in tire_sensor_data
-        # and check if it is greater than 3.0
-        return sum(self.tire_sensor_data) >= 3.0
+class OctoprimeTires(Tires):
+    def __init__(self, tire_wear):
+        self.tire_wear = tire_wear
+
+    def needs_service(self):
+        return sum(self.tire_wear) >= 3.0
